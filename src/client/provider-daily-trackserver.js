8d11d3bb84093b96fd3b742e1c6f3f7af5fe21a8
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-06-20 06:50:32 +0300

    Use basic auth with track server if username and password are set

--- src/client/provider-daily-trackserver.js
+++ src/client/provider-daily-trackserver.js
@@ -1,12 +1,15 @@
 import api from './api'
 
-function initialize({address, vesselId}) {
+function initialize({address, vesselId, username, password}) {
   return {
     queryTracks
   }
 
   function queryTracks(bbox) {
-    return api.get({url: `${address}/${vesselId}/daily-tracks/?bbox=${bbox.toBBoxString()}`})
+    return api.get({
+      url: `${address}/${vesselId}/daily-tracks?bbox=${bbox.toBBoxString()}`,
+      basicAuth: !!username && !!password ? {username, password} : undefined
+    })
   }
 }
 
